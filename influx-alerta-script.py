4276fa4cfd9efe75ce0d3a11a501404a7ef86eed
Muhammed Bakgör <56583366+mbakgor@users.noreply.github.com>
2024-05-10 14:13:08 +0300

    Update influx-alerta-script.py

--- influx-alerta-script.py
+++ influx-alerta-script.py
@@ -1,71 +1,117 @@
 import requests
+import logging
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from datetime import datetime, timezone
+from datetime import datetime
+import pytz
+import sqlite3
 
 from influxdb_client.client.write_api import SYNCHRONOUS
+from dateutil import parser
 
-bucket = "bucket"
-org = "org"
-token = "token"
-influxdb_url = "url"
-alerta_url = "url"
-headers = {'Authorization': 'Key key'}
+bucket = "alerta/90_day"
+org = "YourOrg"
+token = "YourTokenHere"
+influxdb_url = "influx-url"
+alerta_url = "alerta.io-url"
+headers = {'Authorization': 'Key YourAPIKey'}
 
 client = InfluxDBClient(url=influxdb_url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-cache_file = 'alert_ids.txt'
+db_path = '/path/to/db'
+logging.basicConfig(filename='/root/logfile.log', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-def load_cached_ids():
-    try:
-        with open(cache_file, 'r') as file:
-            return set(file.read().split())
-    except FileNotFoundError:
-        return set()
 
-def save_cached_ids(processed_ids):
-    with open(cache_file, 'w') as file:
-        file.write(' '.join(processed_ids))
+def connect_db():
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
 
-processed_ids = load_cached_ids()
 
+def create_table():
+    with connect_db() as conn:
+        conn.execute('''
+            CREATE TABLE IF NOT EXISTS alerts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                alertID TEXT NOT NULL UNIQUE,
+                lastReceiveID TEXT,
+                receiveTime TEXT,
+                updateTime TEXT,
+                customer TEXT,
+                event TEXT,
+                resource TEXT,
+                duplicateCount INTEGER DEFAULT 0,
+                isPushed BOOLEAN DEFAULT FALSE
+            );
+        ''')
+        conn.commit()
 def get_alerts():
-    response = requests.get(alerta_url, headers=headers)
-    print("Status Code:", response.status_code)
-    print("Response Content:", response.text)
-
     try:
-        data = response.json()
-        return data.get('alerts', [])
-    except requests.exceptions.JSONDecodeError:
-        print("Failed to decode JSON from response")
+        response = requests.get(alerta_url, headers=headers)
+        logging.debug(f"Status Code: {response.status_code}")
+        logging.debug(f"Response Content: {response.text}")
+        response.raise_for_status()
+        return response.json().get('alerts', [])
+    except requests.RequestException as e:
+        logging.error(f"Request failed: {e}")
         return []
 
-def send_to_influxdb(alerts):
-    points = []
-    for alert in alerts:
-        alert_id = alert.get('id', '')
-        if alert.get('status') == 'open' and alert_id not in processed_ids:
-            point = Point("alerts")\
-                .field("customer", alert.get('customer', '')) \
-                .tag("customer", alert.get('customer', '')) \
-                .tag("event", alert.get('event', ''))\
-                .tag("resource", alert.get('resource', ''))\
-                .tag("id", alert_id)\
-                .time(datetime.now(timezone.utc), WritePrecision.NS)
+
+def update_db_with_alerts(alerts):
+    with connect_db() as conn:
+        for alert in alerts:
+            alert_id = alert.get('id', '')
+            last_receive_id = alert.get('lastReceiveId', '')
+            receive_time = alert.get('createTime', '')
+            last_receive_time = alert.get('lastReceiveTime', '')
+            cursor = conn.execute('SELECT * FROM alerts WHERE alertID=?', (alert_id,))
+            existing_alert = cursor.fetchone()
+            if existing_alert:
+                conn.execute('''
+                            UPDATE alerts SET
+                                lastReceiveID=?,
+                                updateTime=?,
+                                duplicateCount=duplicateCount+1
+                            WHERE alertID=?;
+                        ''', (last_receive_id, last_receive_time, alert_id))
+            else:
+                conn.execute('''
+                            INSERT INTO alerts (alertID, receiveTime, customer, event, resource)
+                            VALUES (?, ?, ?, ?, ?);
+                        ''', (alert_id, receive_time, alert.get('customer', ''), alert.get('event', ''), alert.get('resource', '')))
+        conn.commit()
+
+
+def send_to_influxdb():
+    with connect_db() as conn:
+        cursor = conn.execute('SELECT * FROM alerts WHERE isPushed=0')
+        points = []
+        for row in cursor:
+            point = Point("alerts") \
+                .field("customer", row['customer']) \
+                .tag("customer", row['customer']) \
+                .tag("event", row['event']) \
+                .tag("resource", row['resource']) \
+                .tag("id", row['alertID']) \
+                .time(int(parser.parse(row['receiveTime']).timestamp() * 1e9), WritePrecision.NS)
             points.append(point)
-            processed_ids.add(alert_id)
-    if points:
-        write_api.write(bucket=bucket, record=points)
-        print("Data written to InfluxDB")
-    save_cached_ids(processed_ids)
+        if points:
+            write_api.write(bucket=bucket, record=points)
+            conn.execute('UPDATE alerts SET isPushed=1 WHERE isPushed=0')
+            conn.commit()
+            logging.info("Data written to InfluxDB")
+
 
 def main():
+    create_table()
     alerts = get_alerts()
     if alerts:
-        send_to_influxdb(alerts)
+        update_db_with_alerts(alerts)
+        send_to_influxdb()
     else:
-        print("No alerts found or invalid response structure.")
+        logging.info("No alerts found or invalid response structure.")
+
 
 if __name__ == "__main__":
     main()
